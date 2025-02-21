adf23844573b7cba09e12d32fc4ced30f943edf9
thiago morgado <thiago_morgado@hotmail.com>
2025-02-20 22:41:15 -0300

    feat: create screen code-otp validation

---
+++ mobile/src/screens/Code-Otp/index.tsx
@@ -0,0 +1,36 @@
+import { ImageBackground, Pressable, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import styles from './styles';
+import Input from '../../components/Input';
+import { useNavigation } from '@react-navigation/native';
+import Button from '../../components/Button';
+import Link from '../../components/Link';
+import Header from '../../components/Header';
+
+export default function ForgotPasswordScreen() {
+  const navigation = useNavigation();
+  return (
+
+    <ImageBackground source={require('../../assets/img/Image.png')} style={styles.container}>
+      <View >
+        <Header title='Esqueceu sua senha' />
+      </View>
+
+      <View style={styles.form}>
+        <View style={styles.formContent}>
+          <Text style={styles.formTitle}>Informe seu email para enviarmos o código de verificação.</Text>
+          <Input placeholder='Email' secureTextEntry={false} />
+        </View>
+
+        <View style={styles.footer}>
+          <Button title='Enviar' onPress={() => alert('Entrar')} />
+          <Link title='Ainda nao tem conta ?' onPress={() => navigation.navigate('CreateAccounts' as never)} />
+        </View>
+
+
+      </View>
+
+
+    </ImageBackground>
+  );
+}
---
+++ mobile/src/screens/Code-Otp/styles.ts
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/theme';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.btnTextColorBlack,
+    justifyContent: 'space-between',
+
+  },
+
+  form: {
+    padding: 25,
+    gap: 10,
+    backgroundColor: theme.colors.primary,
+    height: '80%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    justifyContent: 'space-between',
+  },
+  formContent: {
+    gap: 10,
+  },
+  formTitle: {
+    color: theme.colors.btnTextColorBlack,
+    fontSize: 18,
+    fontFamily: theme.fontsRoboto.Regular,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 20,
+    paddingTop: 86
+  },
+
+  forgotPassword: {
+    color: theme.colors.btnBackground,
+    fontSize: 20,
+    fontFamily: theme.fontsRaleway.Bold,
+    textAlign: 'left',
+  },
+  footer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    height: 79,
+    borderRadius: 50,
+    marginBottom: 50,
+  },
+
+});
+
+export default styles;
